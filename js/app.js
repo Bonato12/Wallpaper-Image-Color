951b088ff68c935f135d7a0966cbb6d499bcf6ef
Sebastian Bonato <sebabonato12@gmail.com>
2019-01-31 18:32:21 -0300

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,50 @@
+console.log("conectado");
+
+
+function cambiarScreen(){
+   document.body.style.backgroundBlendMode = "screen";
+}
+
+function cambiarLuminosity(){
+   document.body.style.backgroundBlendMode = "luminosity";
+}
+
+function cambiarMultiply(){
+   document.body.style.backgroundBlendMode = "multiply";
+}
+
+function cambiarOverlay(){
+   document.body.style.backgroundBlendMode = "overlay";
+}
+
+function cambiarDarken(){
+   document.body.style.backgroundBlendMode = "darken";
+}
+
+function cambiarColorBurn(){
+   document.body.style.backgroundBlendMode = "color-burn";
+}
+
+function cambiarColorDodge(){
+   document.body.style.backgroundBlendMode = "color-dodge";
+}
+
+function cambiarLighten(){
+   document.body.style.backgroundBlendMode = "lighten";
+}
+
+function cambiarDifference(){
+   document.body.style.backgroundBlendMode = "difference";
+}
+
+function cambiarExclusion(){
+   document.body.style.backgroundBlendMode = "exclusion";
+}
+
+function cambiarSaturation(){
+   document.body.style.backgroundBlendMode = "saturation";
+}
+
+function cambiarHardLight(){
+   document.body.style.backgroundBlendMode = "hard-light";
+}
